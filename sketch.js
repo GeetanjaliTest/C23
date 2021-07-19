f4b28c98bfc3568a2a065de7a27d41644cb8acea
GeetanjaliTest <giitanjalim@gmail.com>
2021-07-19 19:03:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,9 +18,10 @@ function setup() {
   canvas = createCanvas(1200, 600);
   engine = Engine.create();
   world = engine.world;
-
-  tower= new Tower(150,350,160,310);
-  ground= new Ground(0,599,2400,2);
+  angle = -PI / 4;
+  ground = new Ground(0, height - 1, width * 2, 1);
+  tower = new Tower(150, 350, 160, 310);
+  cannon = new Cannon(180, 110, 110, 50, angle);
 
   
 
@@ -28,14 +29,25 @@ function setup() {
 
 function draw() {
   background(189);
-
+  image(backgroundImg, 0, 0, width, height);
 
   
 
   Engine.update(engine);
-  tower.display()
-  ground.display()
+  ground.display();
+  
 
+  cannon.display();
+  tower.display();
   
  
 }
+
+
+
+
+
+
+
+
+
